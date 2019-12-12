5ddfca12c8fe1c87f88714d59bb24272d2782b8d
Jakub.Reiter <jakub.reiter.git@listy.internetowe.pl>
2019-12-12 15:50:53 +0100

    run encode

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-# New install
-# svn checkout http://svn.mbnd.eu/svn/dev2/projects/internalwebtools/symfony4_user_skeleton user_sk
-# cd user_sk
-# run ./install.sh
-#key
 read -n1 -r -p "Edit .env - set database parameters... (Press any button when you're done)
 " key
 php composer.phar install
@@ -11,3 +6,5 @@ echo "Loading fixures - for example first, administrative user"
 php bin/console doctrine:schema:update --force --env=dev
 php bin/console doctrine:fixtures:load --env=dev
 yarn install
+yarn run encore dev
+yarn run encore production
